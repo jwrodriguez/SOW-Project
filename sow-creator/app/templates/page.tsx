367352924e7288eb12097f499f293f6a49a51cd4
Teag0029 <jonathan.a.teague-1@ou.edu>
2026-04-16 23:22:49 -0500

    remove content :(

--- sow-creator/app/templates/page.tsx
+++
@@ -1,6 +0,0 @@
-// /templates just redirects to /templates/base (the default view).
-import { redirect } from "next/navigation";
-
-export default function TemplatesPage() {
-  redirect("/templates/base");
-}
